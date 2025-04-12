77c459dd0452b1cab1459fa3bcb524c797d6b7cd
Raj Khare <06rajkhare04@gmail.com>
2025-04-12 20:27:45 +0530

    Added tmux-resurrect to save neovim sessions on disk.

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Documents ~/Projects /home/org -mindepth 1 -maxdepth 2 -type d | fzf)
+    selected=$(find ~/Documents ~/projects /home/org -mindepth 1 -maxdepth 2 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
